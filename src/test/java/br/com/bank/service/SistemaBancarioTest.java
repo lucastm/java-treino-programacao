eff267f2576d38b9af648d087ff762cb63dadfa1
Lucas <lucas.machado@ewally.com.br>
2023-08-26 19:41:08 -0300

    test sistema bancario with bacen mock

--- src/test/java/br/com/bank/service/SistemaBancarioTest.java
+++ src/test/java/br/com/bank/service/SistemaBancarioTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.quality.Strictness;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -38,4 +39,25 @@ class SistemaBancarioTest {
 
         assertEquals(0, sistemaBancario.registrarBanco(banco0));
    }
+
+   @Test
+   public void deve_registrar_banco_com_bacen_mock() {
+        sistemaBancario = new SistemaBancario(bacen);
+        Banco banco1 = new Banco("Um");
+        Mockito.when(bacen.cadastrarBanco(banco1)).thenReturn(1L);
+
+        assertEquals(1, sistemaBancario.registrarBanco(banco1));
+   }
+
+//     BancoNaoCadastradoException do tipo RuntimeException deve ser retornada. Use o assertThrows para isso.
+   @Test
+   public void deve_lancar_execao_quando_falhar_registrar_banco_com_bacen_mock() {
+        sistemaBancario = new SistemaBancario(bacen);
+        Banco banco2 = new Banco("Dois");
+        Mockito.when(bacen.cadastrarBanco(banco2)).thenThrow(new BancoNaoCadastradoException());
+
+        assertThrows(BancoNaoCadastradoException.class, () -> sistemaBancario.registrarBanco(banco2));
+   }
+
+   class BancoNaoCadastradoException extends RuntimeException {}
 }
